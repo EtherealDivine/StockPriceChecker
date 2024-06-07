5aec6cd7065b5c9bfc4e8eb03d262014f5c908f3
EtherealDivine <paprika1440@gmail.com>
2024-06-07 05:05:41 +0000

    renewed

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,24 @@
-'use strict';
+"use strict";
+const express = require("express");
+let mongoose = require("mongoose");
+let mongodb = require("mongodb");
 
 module.exports = function (app) {
+  let uri = process.env.MONGO_URI;
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+  // Removing deprecated options
+  mongoose.connect(uri);
+
+  //create a schema and model to represent the stocks.
+  let stockSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    ips: [String],
+  });
+
+  let Stock = mongoose.model("Stock", stockSchema);
+
+  app.route("/api/stock-prices").get(function (req, res) {
+    // Your code to handle the API route
+  });
 };
